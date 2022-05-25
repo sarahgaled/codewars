bb2543cc870615d73b34298b67c74ac22956296d
sarahgaled <sarajessicagaled@email.com>
2022-05-24 21:37:45 -0400

    fixing function

---
+++ Is-it-even?/index.js
@@ -0,0 +1,13 @@
+function testEven(n){
+    // if n modulo 2 equals to xero
+    // then return true
+    // else
+    // return false
+    if(n % 2 === 0){
+        return true 
+    } else {
+        return false
+    }
+}
+
+console.log(testEven(7))
