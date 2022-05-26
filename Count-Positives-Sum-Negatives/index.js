eb545e022e68f32ad924081c276b125722eee233
sarahgaled <sarajessicagaled@email.com>
2022-05-25 23:48:50 -0400

    taking care of negative sum

--- Count-Positives-Sum-Negatives/index.js
+++ Count-Positives-Sum-Negatives/index.js
@@ -11,15 +11,20 @@ let negativeCount = 0
 // loop through the input array
 for(let i = 0; i < input.length; i++){
 // if the number is greater than 0
-if(input[0] > 0){
+if(input[i] > 0){
 // then count the positive numbers 
 positiveCount++
  }
+ // if the number is less than 0
+ elseif(input[i] < 0)
+ // then add the negative numbers
+     negativeCount += input[0]
+ 
 }
 
 
-// if the number is less than 0
-// then add the negative numbers
+
+
 // return the result with the positive count and sum of negative numbers
 
 }
