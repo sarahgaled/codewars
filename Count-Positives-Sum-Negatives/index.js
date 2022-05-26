34341cbc803f6e5c30e5a4943cf916059b9481c7
sarahgaled <sarajessicagaled@email.com>
2022-05-25 23:42:39 -0400

    first if statement

---
+++ Count-Positives-Sum-Negatives/index.js
@@ -0,0 +1,17 @@
+function countPositivesSumNegatives(input){
+// if the input is null empty or 0
+if(input === null || input.length === 0){
+// return an empty array
+}
+
+// create a result array
+// create variable to store the positive count
+// and a variable to store the negative count
+// loop through the input array
+// if the number is greater than 0
+// then count the positive numbers 
+// if the number is less than 0
+// then add the negative numbers
+// return the result with the positive count and sum of negative numbers
+
+}
