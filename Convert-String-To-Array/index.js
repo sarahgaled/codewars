8a535c6ea2f7b61e55d5fdd40a3ae12cb686b081
sarahgaled <sarajessicagaled@email.com>
2022-05-26 17:36:37 -0400

    creating the function

--- Convert-String-To-Array/index.js
+++ Convert-String-To-Array/index.js
@@ -2,3 +2,6 @@
 // take the string and use the split method
 // split the string by implementing a space in betweek the quotes to capture the whole word
 // return - and it split method returns an array
+function stringToArray(string){
+
+}
