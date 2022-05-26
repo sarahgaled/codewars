df421b6573192cbc67e8ba3b0f34548bdd02eaed
sarahgaled <sarajessicagaled@email.com>
2022-05-26 11:38:05 -0400

    coding the operation

--- Grasshopper-Basic-Function-Fixer/index.js
+++ Grasshopper-Basic-Function-Fixer/index.js
@@ -1,5 +1,5 @@
 // I created this function to add five to any number that was passed in to it and return the new value. It doesnt throw any errors but it returns the wrong number
 // Can you help fix the function
 function addFive(num){
-
+    var total = num + 5
 }
