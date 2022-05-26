000ef4618ebeca84e28cf05adb337412de34a7ad
sarahgaled <sarajessicagaled@email.com>
2022-05-26 10:29:08 -0400

    psuedo code

--- Third-Angle-Triangle/index.js
+++ Third-Angle-Triangle/index.js
@@ -1,5 +1,7 @@
 function otherAngle(a, b){
-    
+    // create a variable to store the result of the addition of the angles a & b
+    // create another variable to store the result of the addition of the two angles and subtract that answer from 180 degrees
+    // return that result
     
     
     
