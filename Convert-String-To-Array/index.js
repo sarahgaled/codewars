5d066676f0ec98f2ba5e1990ff848f44057cd870
sarahgaled <sarajessicagaled@email.com>
2022-05-26 17:35:58 -0400

    pseudo code

--- Convert-String-To-Array/index.js
+++ Convert-String-To-Array/index.js
@@ -1,1 +1,4 @@
 // write a function to split a string and convert it into an array of words
+// take the string and use the split method
+// split the string by implementing a space in betweek the quotes to capture the whole word
+// return - and it split method returns an array
