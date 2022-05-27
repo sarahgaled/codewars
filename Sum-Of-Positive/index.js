26825c4272be93d420e34b41942de46d1e97b285
sarahgaled <sarajessicagaled@email.com>
2022-05-26 23:00:40 -0400

    creating the variable to store the sum

--- Sum-Of-Positive/index.js
+++ Sum-Of-Positive/index.js
@@ -1,9 +1,10 @@
 // you get an array of numbers, return the sum of all the positive ones
 // create a function and pass in an array
 function positiveSum(arr){
-
-}
 // create a variable to store the sum of the positive numbers
+let sum = 0
+}
+
 // iterate through the array and check
 // if the element is greater than 0
 // add it to the variable sum
