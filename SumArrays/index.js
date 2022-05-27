c9d6aa845a2830f6617a8d8a04807f2ecb5995b3
sarahgaled <sarajessicagaled@email.com>
2022-05-26 22:41:58 -0400

    instructions

---
+++ SumArrays/index.js
@@ -0,0 +1,2 @@
+// write a function that takes an array of numbers and returns the sum of the numbers. 
+// the numbers can be negative or non integer. if the array does not contain any numbers, then return 0
