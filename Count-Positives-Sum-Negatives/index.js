f5e1a66d8cd43b8d20f59f6c3cba87ccf3d982ad
sarahgaled <sarajessicagaled@email.com>
2022-05-26 23:05:01 -0400

    fixing format

--- Count-Positives-Sum-Negatives/index.js
+++ Count-Positives-Sum-Negatives/index.js
@@ -1,26 +1,25 @@
-function countPositivesSumNegatives(input){
-// if the input is null empty or 0
-if(input === null || input.length === 0){
-// return an empty array
-return []
-}
-// create variable to store the positive count
-let positiveCount = 0
-// and a variable to store the negative count
-let negativeCount = 0
-// loop through the input array
-for(let i = 0; i < input.length; i++){
-// if the number is greater than 0
-if(input[i] > 0){
-// then count the positive numbers 
-positiveCount++
- }
- // if the number is less than 0
- elseif(input[i] < 0)
- // then add the negative numbers
-     negativeCount += input[0]
- 
-}
-// return the array with the positive count and sum of negative numbers
-return [positiveCount, negativeCount]
+function countPositivesSumNegatives(input) {
+  // if the input is null empty or 0
+  if (input === null || input.length === 0) {
+    // return an empty array
+    return [];
+  }
+  // create variable to store the positive count
+  let positiveCount = 0;
+  // and a variable to store the negative count
+  let negativeCount = 0;
+  // loop through the input array
+  for (let i = 0; i < input.length; i++) {
+    // if the number is greater than 0
+    if (input[i] > 0) {
+      // then count the positive numbers
+      positiveCount++;
+    }
+    // if the number is less than 0
+    elseif(input[i] < 0);
+    // then add the negative numbers
+    negativeCount += input[0];
+  }
+  // return the array with the positive count and sum of negative numbers
+  return [positiveCount, negativeCount];
 }
