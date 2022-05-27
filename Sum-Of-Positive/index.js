bac2072bdf0a515737eb249a8e81f301c12bc85d
sarahgaled <sarajessicagaled@email.com>
2022-05-26 23:00:04 -0400

    building the function

--- Sum-Of-Positive/index.js
+++ Sum-Of-Positive/index.js
@@ -1,5 +1,8 @@
 // you get an array of numbers, return the sum of all the positive ones
 // create a function and pass in an array
+function positiveSum(arr){
+
+}
 // create a variable to store the sum of the positive numbers
 // iterate through the array and check
 // if the element is greater than 0
