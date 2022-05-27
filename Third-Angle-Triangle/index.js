f710b3ccb7b9fedc1ba4350c734c0ac813465ab1
sarahgaled <sarajessicagaled@email.com>
2022-05-26 23:04:18 -0400

    fixing format

--- Third-Angle-Triangle/index.js
+++ Third-Angle-Triangle/index.js
@@ -1,8 +1,8 @@
-function otherAngle(a, b){
-    // create a variable to store the result of the addition of the angles a & b
-    let c = (a + b)
-    // create another variable to store the result of the addition of the two angles and subtract that answer from 180 degrees
-    let d = (180 - c)
-    // return that result
-    return d;
+function otherAngle(a, b) {
+  // create a variable to store the result of the addition of the angles a & b
+  let c = a + b;
+  // create another variable to store the result of the addition of the two angles and subtract that answer from 180 degrees
+  let d = 180 - c;
+  // return that result
+  return d;
 }
