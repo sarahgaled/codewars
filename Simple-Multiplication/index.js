6e6b5805445be4390fd290c15c1e159815a86ad4
sarahgaled <sarajessicagaled@email.com>
2022-05-26 23:05:42 -0400

    fixing format

--- Simple-Multiplication/index.js
+++ Simple-Multiplication/index.js
@@ -1,13 +1,13 @@
 // multiply a given number by eight if it is even number and by nine otherwise
 // set up a function and pass a variable for the given number
-function simpleMultiplication(number){
-// create an if statement that takes care of if eight is modulo 2 === 0
-if(number % 2 === 0){
-// then multiply the given number by 8
-return number * 8
-} // else multiply that number by 9 
-else {
-    // and return 
-    return number * 9
-}
+function simpleMultiplication(number) {
+  // create an if statement that takes care of if eight is modulo 2 === 0
+  if (number % 2 === 0) {
+    // then multiply the given number by 8
+    return number * 8;
+  } // else multiply that number by 9
+  else {
+    // and return
+    return number * 9;
+  }
 }
