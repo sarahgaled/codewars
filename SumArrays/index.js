9f50590710f9c2fa470949a06994a4da5c5cdedd
sarahgaled <sarajessicagaled@email.com>
2022-05-26 22:43:18 -0400

    pseudo code

--- SumArrays/index.js
+++ SumArrays/index.js
@@ -1,2 +1,6 @@
 // write a function that takes an array of numbers and returns the sum of the numbers. 
 // the numbers can be negative or non integer. if the array does not contain any numbers, then return 0
+// create a variable result and let it = 0
+// iterate through the array
+// add all the elements of the array to the result variable
+// then return result
