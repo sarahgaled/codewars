adff5468fbf533e02c1c491edde17718825afe67
sarahgaled <sarajessicagaled@email.com>
2022-05-26 23:01:20 -0400

    iterating through the array

--- Sum-Of-Positive/index.js
+++ Sum-Of-Positive/index.js
@@ -3,9 +3,13 @@
 function positiveSum(arr){
 // create a variable to store the sum of the positive numbers
 let sum = 0
+// iterate through the array and check
+for(let i = 0; i < arr.length; i++){
+    
+}
 }
 
-// iterate through the array and check
+
 // if the element is greater than 0
 // add it to the variable sum
 // return the sum
