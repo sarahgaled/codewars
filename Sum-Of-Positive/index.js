391c8f5f9de46d15d657631c0e59a0544cdfe400
sarahgaled <sarajessicagaled@email.com>
2022-05-26 22:57:24 -0400

    pseudo code

--- Sum-Of-Positive/index.js
+++ Sum-Of-Positive/index.js
@@ -1,1 +1,7 @@
 // you get an array of numbers, return the sum of all the positive ones
+// create a function and pass in an array
+// create a variable to store the sum of the positive numbers
+// iterate through the array and check
+// if the element is greater than 0
+// add it to the variable sum
+// return the sum
