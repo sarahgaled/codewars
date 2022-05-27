698ae71b7c27a109f4bb141f0e6ebe7763b8ac3d
sarahgaled <sarajessicagaled@email.com>
2022-05-26 22:44:55 -0400

    creating a result variable

--- SumArrays/index.js
+++ SumArrays/index.js
@@ -2,9 +2,10 @@
 // the numbers can be negative or non integer. if the array does not contain any numbers, then return 0
 
 function sum(numbers){
-    
-}
 // create a variable result and let it = 0
+let result = 0
+}
+
 // iterate through the array
 // add all the elements of the array to the result variable
 // then return result
