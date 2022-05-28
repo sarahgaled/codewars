b8f58f8113f1bf51f897662e307c5c0f4d056907
sarahgaled <sarajessicagaled@email.com>
2022-05-27 20:09:07 -0400

    building the function

--- Convert-Number-Reversed-Array/index.js
+++ Convert-Number-Reversed-Array/index.js
@@ -1,2 +1,5 @@
 // Convert Number to Reversed Array of Digits
 // Given a random non negative number, you have to return the digits of this number within an array in reversed order
+function digitize(n){
+
+}
