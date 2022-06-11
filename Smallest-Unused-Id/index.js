db8ac08885b614491671009848a923b85eba33fe
sarahgaled <sarajessicagaled@email.com>
2022-06-11 19:07:46 -0400

    smallest unused Id

--- Smallest-Unused-Id/index.js
+++ Smallest-Unused-Id/index.js
@@ -5,15 +5,12 @@ Note: The given array of used IDs may be unsorted. For test reasons there may be
 
 function nextId(ids){
     // use a for loop to loop through the ids in the ids array
-      for(let i = 0; i < ids.length; i++){
+      for(let i = 0; i < ids.length + 1; i++){
         //   then use an if statement to check if the current index in the ids array === -1, if it does return the current element
         if(ids.indexOf(i) === -1){
-          return i
-          console.log(i)
+          return i 
       }
      }
-      return ids.length  
-      console.log(ids.length)
     }
 
     console.log(nextId([0,1,2,3,5]))
