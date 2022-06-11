c12adc3e5307b8a579574455cd74a816d7f16294
sarahgaled <sarajessicagaled@email.com>
2022-06-11 19:18:13 -0400

    count odd numbers below n

--- Count-Odd-Numbers-Below-N/index.js
+++ Count-Odd-Numbers-Below-N/index.js
@@ -2,34 +2,14 @@
 // Examples: 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
 // 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
 
+
+
+
 // create the function oddCount and pass in n
-// function oddCount(n){
-//     // declare a variable to store the result
-//     let result = 0
-//     // use a for loop to iterate through n
-//     for(let i = 1; i <= n; i++){
-//         // check to see if n is odd
-//         if(n % 2 === 1){
-//             // divide the result by 2 and round down
-//             result = Math.floor(n/2)
-//         }
-//     }
-//     return result
-// }
-
-
 function oddCount(n){
+  // use the Math.floor method to round down to the nearest integer and divide by 2
+    let result = Math.floor(n/2)
   
-    //   Declare a vraiable to hold the result
-      let result = 0
-    // use for loop to iterate through n
-      for(let i = 1; i < n; i++){
-        //   if statement to check if n is odd
-    //     if(n % 2 === 1){
-    //       if it is indeed odd then take n and divide it by 2
-          result = Math.floor(n/2)
-    //     }
-      }
      return result
     }
 
